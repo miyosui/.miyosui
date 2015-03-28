c4a5c1c2b384f0114d8e423f20223e471e1902b3
Sakichi Miyosui <sakichi.miyosui@gmail.com>
2015-03-28 23:00:42 +0000

    bin in root
    Move bin to root of repo.
    Every file symlinked should be on the root of repo for the time being.

---
+++ src/lib/common.rb
@@ -0,0 +1,70 @@
+require 'pathname'
+# TODO
+# Move module to lib folder
+# Test methods
+#
+# Will not do it now.
+# How will realpath behave?
+module Common
+  REAL_FILEPATH = "#{Pathname.new(__FILE__).realpath}"
+  HOME = ENV['HOME']
+  COMMON_FILES = %w(.bash_profile .bashrc .gitconfig .profile .tmux.conf .vim .vimrc bin)
+
+  class << self
+    # real absolute path for given common file.
+    def common_path(common_file)
+      Pathname.new("../../../../#{common_file}").expand_path(REAL_FILEPATH)
+    end
+    private :common_path
+
+    # run code based on common files
+    def common_files(&block)
+      COMMON_FILES.each do |common_file|
+        case block.arity
+        when 1
+          yield new(common_file)
+        when 2
+          yield new(common_file), common_path(common_file)
+        end
+      end
+    end
+    private :common_files
+
+    def lib
+      "#{Pathname.new("../../lib").expand_path(REAL_FILEPATH)}"
+    end
+    public :lib
+
+    # Name of symlink to be created/removed
+    def new(common_file)
+      Pathname.new("#{HOME}/#{common_file}")
+    end
+    private :new
+
+    # Remove new files even if they are not symlinks.
+    def remove!
+      common_files do |new| 
+        print "Removing file #{new}..." if $VERBOSE
+        new.delete 
+        puts "done." if $VERBOSE
+      end
+    end
+    public :remove!
+
+    # Setup symlink files. Displays error message if file exists.
+    def setup
+      common_files do |new, common_path| 
+        begin
+          print "Creating symlink from #{new} to #{common_path}..." if $VERBOSE
+          new.make_symlink(common_path) 
+          puts "done." if $VERBOSE
+        rescue Errno::EEXIST
+          puts "\n\t#{$!.message}" if $VERBOSE
+        end
+      end
+    end
+    public :setup
+  end
+end
+
+
